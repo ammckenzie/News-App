d14b5e7608ae2bdb9e863a9822fcb306f0b20d9f
Angela McKenzie <angelamckenzie@Angelas-MacBook-Pro.local>
2014-11-02 21:55:17 -0500

    For 11/3
    - Worked on toggles
    - Finished search bar
    - “News Feed” for title (?)

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,74 @@ $(document).ready(function() {
         	$('.article-container').css('position','relative');
        });
 
+
+
+
+        /* Search Bar JS */
+
+        function hoverClickLeft() {
+            $('#slide-left').removeClass('slide-left-secondary');
+            if ($('#slide-left').hasClass('slid-left')) {} else {
+                $('#slide-left').addClass("slid-left");
+                $('input[name=search1]').show();
+                $('input[name=search1]').focus();
+            }
+            if ($('#slide-right').hasClass('slid-right')) {
+                $('#slide-right').removeClass('slid-right');
+                $('input[name=search2]').hide();
+            } else {}
+            $('#search-text').addClass('search-transition');
+    };
+
+      // $('.searchslide').click(function() {
+      //           $('.title').hide()
+      //   });
+
+
+
+    function hoverClickRight() {
+        if ($('#slide-right').hasClass('slid-right')) {} else {
+            $('#slide-right').addClass("slid-right");
+            $('input[name=search2]').show();
+            $('input[name=search2]').focus();
+        }
+        if ($('#slide-left').hasClass('slid-left')) {
+            $('#slide-left').removeClass('slid-left');
+            $('input[name=search1]').hide();
+        } else {}
+        $('#slide-left').addClass('slide-left-secondary');
+        $('#search-text').addClass('search-transition');
+    };
+
+
+    $(document).ready(function () {
+        $('input[name=search1]').hide();
+        $('input[name=search2]').hide();
+        $('#slide-left').on('click', function () {
+            hoverClickLeft();
+        });
+        $('#slide-right').on('click', function () {
+            hoverClickRight();
+        });
+        $('#search-text').on('click', function() {
+            hoverClickLeft();
+        });
+        $(document).on('click', function (event) {
+            if (!$(event.target).closest('.wrapper').length) {
+                $('#slide-left').removeClass('slid-left');
+                $('#slide-right').removeClass('slid-right');
+                $('#slide-left').removeClass('slide-left-secondary');
+                $('input[name=search1]').hide();
+                $('input[name=search2]').hide();
+                $('#search-text').removeClass('search-transition');
+            }
+        });
+
+    });
+
+
+
+
         
 
     });
